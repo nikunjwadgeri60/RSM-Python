a988616229af6a336e2c7a0c162c7bc6ca40e70a
Nikunj Wadgeri <91813969+nikunjwadgeri60@users.noreply.github.com>
2022-01-09 12:43:36 +0530

    Add files via upload

---
+++ JAVATPOINT_Q1.py
@@ -0,0 +1,6 @@
+st = int(input('speed of train in kmph: '))
+st = st * (5 / 18)
+print('speed of train in m/s is: ', st)
+tt = int(input('time taken by train to cross the pole: '))
+lot = st * tt
+print('length of train in meters is: ', lot)
