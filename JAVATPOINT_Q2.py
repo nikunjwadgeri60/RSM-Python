548c5545bc3e1fe6df88a8bb9d4366fc00aa8f77
Nikunj Wadgeri <91813969+nikunjwadgeri60@users.noreply.github.com>
2022-01-09 10:41:26 +0530

    Add files via upload

---
+++ JAVATPOINT_Q2.py
@@ -0,0 +1,6 @@
+tl = int(input("enter train length: "))
+ms = int(input("enter man's speed: "))
+tt = int(input("enter time taken to cross the man: "))
+rs = tl / tt * (18 / 5)
+ts = rs + ms
+print("train speed in kmph :", ts)
